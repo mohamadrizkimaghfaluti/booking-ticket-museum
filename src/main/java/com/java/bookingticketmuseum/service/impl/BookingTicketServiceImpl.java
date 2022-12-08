47deb74673f0e1a788bc0021f9d9a6ca5bebcfb2
mohamadrizki <mohamadrizkimaghfaluti@gmail.com>
2022-12-08 12:42:39 +0700

    create project

--- src/main/java/com/java/bookingticketmuseum/service/impl/BookingTicketServiceImpl.java
+++ src/main/java/com/java/bookingticketmuseum/service/impl/BookingTicketServiceImpl.java
@@ -13,10 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
